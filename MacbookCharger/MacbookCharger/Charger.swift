4c45e951281e1b65a9bdad036506e5f48f60744d
Paul <phs880623@gmail.com>
2022-02-17 22:27:50 +0900

    feat: 애플, 맥북, 아이패드 충전기 제작

---
+++ MacbookCharger/MacbookCharger/Charger.swift
@@ -0,0 +1,41 @@
+//
+//  Charger.swift
+//  MacbookCharger
+//
+//  Created by 박형석 on 2022/02/17.
+//
+
+import Foundation
+
+struct IphoneCharger: ChargerProtocol {
+    var chargemAhPerHour: Double
+    
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < self.chargemAhPerHour {
+            return chargeablemAhPerHour
+        }
+        return self.chargemAhPerHour
+    }
+}
+
+struct MackBookCharger: ChargerProtocol {
+    var chargemAhPerHour: Double
+    
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < self.chargemAhPerHour {
+            return chargeablemAhPerHour
+        }
+        return self.chargemAhPerHour
+    }
+}
+
+struct IPadCharger: ChargerProtocol {
+    var chargemAhPerHour: Double
+    
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < self.chargemAhPerHour {
+            return chargeablemAhPerHour
+        }
+        return self.chargemAhPerHour
+    }
+}
